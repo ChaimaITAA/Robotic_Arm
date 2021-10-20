453f3697ab29ce972d9ed63c3faad4e78f89a663
chaimaforever <chaima-soui@hotmail.com>
2021-10-20 19:38:48 +0100

    code

--- catkin_ws/build/one/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chaima/catkin_ws/src/one
-# Build directory: /home/chaima/catkin_ws/build/one
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/tutorial_one/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chaima/catkin_ws/src/tutorial_one
-# Build directory: /home/chaima/catkin_ws/build/tutorial_one
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
